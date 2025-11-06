7a7d3fbb2a9fae15842caa5bd9df31cc83b2c8a4
Udemy - My Nhung <mynhungttm.work@gmail.com>
2025-11-06 23:19:48 +0700

    feat: add ex3 lesson 2

---
+++ tests/lesson-2/ex3.js
@@ -0,0 +1,9 @@
+const numberInput = Number(0);
+
+if (numberInput > 0) {
+  console.log("Giá trị bạn nhập là số dương");
+} else if (numberInput < 0) {
+  console.log("Giá trị bạn nhập là số âm");
+} else {
+  console.log("Giá trị bạn nhập là số 0");
+}
