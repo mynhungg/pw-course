14b6f434585c636f82393e4a5b46b6e3e39266cc
Udemy - My Nhung <mynhungttm.work@gmail.com>
2025-11-07 00:06:31 +0700

    feat: add ex4 lesson 2

--- tests/lesson-2/ex3.js
+++ tests/lesson-2/ex3.js
@@ -1,8 +1,8 @@
-const numberInput = Number(0);
+const NUMBER_INPUT = Number(0);
 
-if (numberInput > 0) {
+if (NUMBER_INPUT > 0) {
   console.log("Giá trị bạn nhập là số dương");
-} else if (numberInput < 0) {
+} else if (NUMBER_INPUT < 0) {
   console.log("Giá trị bạn nhập là số âm");
 } else {
   console.log("Giá trị bạn nhập là số 0");
---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+let heightInput = Number(prompt("Input your height(cm): "));
+
+let modHeight = heightInput % 100;
+
+if (isNaN(heightInput) || heightInput <= 0) {
+  console.log("Please enter a positive number greater than 0");
+}
+
+let idealWeight = (modHeight * 9) / 10;
+let maxWeight = modHeight;
+let minWeight = (modHeight * 8) / 10;
+
+console.log(
+  "With ",
+  heightInput,
+  "cm, your ideal, max and min weight sequentially are: ",
+  idealWeight,
+  "kg ",
+  maxWeight,
+  "kg ",
+  minWeight,
+  "kg"
+);
